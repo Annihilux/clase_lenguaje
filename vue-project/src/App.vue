333ae41203e62d37169c50897045e9f214a19d01
MiguelNotario <annihilux20@gmail.com>
2024-11-11 20:12:20 +0100

    Ya puedo seguir la clase desde aquí

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Principal from './components/Principal.vue';
+</script>
+
+<template>
+  <h1> MI PRIMERA APP </h1><br>
+    
+  <Principal></Principal>
+
+
+</template>
+
+<style scoped>
+  .contenedores{
+    background-color: red;
+  }
+</style>
