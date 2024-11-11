b9519a00d316bd8b8925cf8289fb097343c931e1
MiguelNotario <annihilux20@gmail.com>
2024-11-11 20:24:15 +0100

    Ahora si que si, ya puedo seguir la clase desde aqui, por fin

---
+++ vue-project-clase/src/components/Principal.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {ref} from 'vue';
+import Login from './auth/Login.vue';
+import Register from './auth/Register.vue';
+
+const blLoginVisible=ref(true);
+const blRegistroVisible=ref(false);
+
+
+function mostrarRegistro(){
+  blLoginVisible.value=false;
+  blRegistroVisible.value=true;
+}
+
+function mostrarLogin(){
+  blLoginVisible.value=true;
+  blRegistroVisible.value=false;
+}
+
+function cancelar(){
+  blLoginVisible.value=true;
+  blRegistroVisible.value=false;
+}
+
+
+</script>
+
+<template>
+  
+
+  <Login v-if="blLoginVisible" @logeado="mostrarLogin" @solicitarRegistro="mostrarRegistro" @cancelar="cancelar"></Login>
+  <Register v-if="blRegistroVisible" @cancelar="cancelar"></Register>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
