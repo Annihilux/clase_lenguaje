a7f1da9dc944d17f37478d822407eea6ed0b0862
Miguel <Miguel>
2024-11-21 13:25:41 +0100

    Configuracion firebase

--- vue-project-clase/src/App.vue
+++ vue-project-clase/src/App.vue
@@ -1,5 +1,18 @@
 <script setup>
 import Principal from './components/Principal.vue';
+import { initializeApp } from 'firebase/app';
+//import { getFirestore, collection } from 'firebase/firestore'
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAOprvi2YHvaV21yx_VUvRpzuI7wj0iDkg",
+  authDomain: "ejemplo-564bb.firebaseapp.com",
+  projectId: "ejemplo-564bb",
+  storageBucket: "ejemplo-564bb.firebasestorage.app",
+  messagingSenderId: "169970429584",
+  appId: "1:169970429584:web:0789104ea35549afbe7215",
+  measurementId: "G-NCL6GKT4N8"
+})
+
 </script>
 
 
